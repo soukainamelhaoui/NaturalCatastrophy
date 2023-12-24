ce6a2f55289af28f99961f30311e4338a9214934
taflur <melhaouisoukaina1@gmail.com>
2023-12-24 16:49:31 +0100

    initial commit

---
+++ config/src/test/java/ma/fstt/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.fstt.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
